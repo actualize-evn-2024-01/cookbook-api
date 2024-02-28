f20306c710a0abade64e287e424f2816a3c2a1c8
Peter Jang <peter.x.jang@gmail.com>
2024-02-27 18:51:04 -0600

    Add RESTful recipes create action (with hard-coded values)

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,18 @@ class RecipesController < ApplicationController
     render :index
   end
 
+  def create
+    @recipe = Recipe.create(
+      title: "Mud Pie",
+      chef: "Peter Jang",
+      image_url: "test.jpg",
+      prep_time: 10,
+      ingredients: "Mud",
+      directions: "Bake the mud",
+    )
+    render :show
+  end
+
   def show
     @recipe = Recipe.find_by(id: params[:id])
     render :show
