630474fe393bceac4ae737413c4d7be931880279
Peter Jang <peter.x.jang@gmail.com>
2024-02-27 19:38:37 -0600

    Add automated tests for recipes index/create/show

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "index" do
+    get "/recipes.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Recipe.count, data.length
+  end
+
+  test "create" do
+    assert_difference "Recipe.count", 1 do
+      post "/recipes.json", params: { title: "Cake", chef: "Peter", image_url: "test.jpg", prep_time: 10, ingredients: "Batter", directions: "Bake" }
+      assert_response 200
+    end
+  end
+
+  test "show" do
+    get "/recipes/#{Recipe.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "title", "chef", "ingredients", "directions", "image_url", "prep_time", "created_at", "updated_at"], data.keys
+  end
 end
