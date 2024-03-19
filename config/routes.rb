601e0bc01473707c7501e23f057a2569a1e09c86
Peter Jang <peter.x.jang@gmail.com>
2024-03-18 19:13:55 -0500

    Implement signup (create a new user)

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  post "/users" => "users#create"
+
   get "/recipes" => "recipes#index"
   post "/recipes" => "recipes#create"
   get "/recipes/:id" => "recipes#show"
